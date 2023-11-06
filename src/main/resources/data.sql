f3de3dde351f1196f93f4f7405e8d00301e63d22
anuamreddy47486@gmail.com <anumareddy47486@gmail.com>
2023-11-06 11:42:51 -0600

    Adding SQL file insert values into H2 DB to check it's functionality and connection

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into USER_DETAILS(DOB,ID,NAME) values (CURRENT_DATE,1001,'Anuma');
+insert into USER_DETAILS(DOB,ID,NAME) values (CURRENT_DATE,1002,'Ajith');
+insert into USER_DETAILS(DOB,ID,NAME) values (CURRENT_DATE,1003,'Abhi');
+insert into USER_DETAILS(DOB,ID,NAME) values (CURRENT_DATE,1004,'Raju');
+insert into USER_DETAILS(DOB,ID,NAME) values (CURRENT_DATE,1005,'KAju');
